992dddecfef5af8e19605864b985ed63978bc58b
simonchamorro <simon.chamorro@usherbrooke.ca>
2019-07-05 10:27:20 -0400

    fix intersetions and street segments and plot

--- scripts/plot_graph.py
+++ scripts/plot_graph.py
@@ -6,48 +6,53 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import subprocess
 import argparse
+import pandas as pd
 import collections
 
 parser = argparse.ArgumentParser("Creates panoramas from vuze pictures")
-parser.add_argument('--input_path', type=str, help='graph path')
+parser.add_argument('--input_path', type=str, help='Input path')
+parser.add_argument('--zones', action='store_true', help='Different color for streets and corners')
 args = parser.parse_args()
 
 print("Path : " + args.input_path)
 input_path = args.input_path
+zones = args.zones
 
-G = nx.read_gpickle(input_path)
+G = nx.read_gpickle(input_path + '/graph.pkl')
+
 pos = {k: v.get("coords")[0:2] for k, v in G.nodes(data=True)}
+if zones:
+    meta_df = pd.read_hdf(input_path + "/meta.hdf5", key="df", index=False)
+    corners = [node for node in G.nodes if node in meta_df[meta_df.type == 'intersection'].frame]
+    streets = [node for node in G.nodes if node in meta_df[meta_df.type == 'street_segment'].frame]
+    other = [node for node in G.nodes if node not in meta_df[meta_df.type == 'street_segment'].frame]
+    other = [node for node in other if node not in meta_df[meta_df.type == 'intersection'].frame]
 
-# nx.draw_networkx_nodes(G, pos,
-#                        nodelist=G.nodes,
-#                        node_color='r',
-#                        node_size=10,
-#                        alpha=0.8,
-#                        with_label=True)
+    nx.draw_networkx_nodes(G, pos,
+                           nodelist=corners,
+                           node_color='#e23f3a',
+                           node_size=1,
+                           alpha=0.8,
+                           with_label=True)
 
-# # First node is Green
-# nx.draw_networkx_nodes(G, pos,
-#                        nodelist={15},
-#                        node_color='g',
-#                        node_size=50,
-#                        alpha=0.8)
+    nx.draw_networkx_nodes(G, pos,
+                           nodelist=streets,
+                           node_color='#000cda',
+                           node_size=1,
+                           alpha=0.8)
 
-# # Second node is blue
-# nx.draw_networkx_nodes(G, pos,
-#                        nodelist={16},
-#                        node_color='b',
-#                        node_size=50,
-#                        alpha=0.8)
-# edges = nx.draw_networkx_edges(G, pos=pos)
+    nx.draw_networkx_nodes(G, pos,
+                           nodelist=other,
+                           node_color='g',
+                           node_size=20,
+                           alpha=0.8)
+    edges = nx.draw_networkx_edges(G, pos=pos)
 
-# nx.draw_networkx(G, pos,
-#                  nodelist=G.nodes,
-#                  node_color='r',
-#                  node_size=10,
-#                  alpha=0.8,
-#                  with_label=True)
-nx.draw(G, pos,node_color='r', node_size=1)
+else:
+    nx.draw(G, pos,node_color='r', node_size=1)
+
 plt.axis('equal')
+plt.savefig('graph.png', transparent=True, dpi=1000)
 plt.show()
 
 print("num_edges: "+ str(len(G.edges)))
