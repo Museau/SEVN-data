263e05d86809ad3b1bb1e9822ef11b770cf53d6c
Museau <margaux.luck@gmail.com>
2019-08-11 12:36:46 +0200

    create step 4 bash script

---
+++ scripts/04_stitch_reconstructions.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+POSES_FOLDER='data/SEVN/raw/poses'
+
+
+# Export conda functions in subshell.
+source ~/miniconda3/etc/profile.d/conda.sh
+# Activate conda env.
+conda activate SEVN-data
+
+
+# Extracts camera positions from ORBSLAM2 .txt files and stitch them together.
+python scripts/04_stitch_reconstructions.py --input_dir=$POSES_FOLDER --output_dir=$POSES_FOLDER
