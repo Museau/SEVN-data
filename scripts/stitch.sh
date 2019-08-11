fb3d5df9acf8a0842ed33ade5a71e81dcb1b2e2c
Museau <margaux.luck@gmail.com>
2019-08-11 11:50:24 +0200

    add comments

--- scripts/stitch.sh
+++ scripts/stitch.sh
@@ -1,16 +1,26 @@
 #!/bin/sh
-# $1 = input dir
-# $2 = output dir
-# $3 = frame num
+# $1=input_dir
+# $2=output_dir
+# $3=frame_num
 
-pto_gen -o "$2/pano_$3.pto" -p 2 -f 120 "$1/image_1/$3.png" "$1/image_3/$3.png" "$1/image_5/$3.png" "$1/image_7/$3.png"
-cpfind -o "$2/pano_$3.pto" --multirow --celeste "$2/pano_$3.pto"
-cpclean -o "$2/pano_$3.pto" "$2/pano_$3.pto"
-linefind -o "$2/pano_$3.pto" "$2/pano_$3.pto"
-autooptimiser -a -m -l -s -o "$2/pano_$3.pto" "$2/pano_$3.pto"
-pano_modify --canvas=AUTO --crop=AUTO -o "$2/pano_$3.pto" "$2/pano_$3.pto"
-hugin_executor --stitching --prefix="$2/pano_$3" "$2/pano_$3.pto"
-convert "$2/pano_$3.tif" "$2/pano_$3.png"
+# Assembles a Hugin .pto project file using equirectangular projection.
+pto_gen -o '$2/pano_$3.pto' -p 2 -f 120 '$1/camera_1/$3.jpg' '$1/camera_3/$3.jpg' '$1/camera_5/$3.jpg' '$1/camera_7/$3.jpg'
+# Control point detector for hugin.
+cpfind --multirow --celeste -o '$2/pano_$3.pto' '$2/pano_$3.pto'
+# Remove all non-credible control points.
+cpclean -o '$2/pano_$3.pto' '$2/pano_$3.pto'
+# Find vertical lines and assigns vertical control points to them.
+linefind -o '$2/pano_$3.pto' '$2/pano_$3.pto'
+# Control point optimization.
+autooptimiser -a -m -l -s -o '$2/pano_$3.pto' '$2/pano_$3.pto'
+# Change some output options of the project file
+pano_modify --canvas=AUTO --crop=AUTO -o '$2/pano_$3.pto' '$2/pano_$3.pto'
+# Stitching panorama
+hugin_executor --stitching --prefix='$2/pano_$3' '$2/pano_$3.pto'
+
+# Convert .tif to .jpg
+convert "$2/pano_$3.tif" "$2/pano_$3.jpg"
+
+# Remove unwanted format (.tif and .pto)
 rm "$2/pano_$3.tif"
 rm "$2/pano_$3.pto"
-
