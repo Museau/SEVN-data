0cbd5445cfce0017eb51368e3fe0796bfc3b3923
Museau <margaux.luck@gmail.com>
2019-08-11 15:42:12 +0200

    add parse_args/main/comments

--- scripts/rename_panos.py
+++ scripts/rename_panos.py
@@ -1,21 +1,41 @@
 import os
+
 import argparse
+import glob
 
 
-parser = argparse.ArgumentParser("Renames images")
-parser.add_argument('input_path', type=str, help='Path to folder containing the images to rename')
-parser.add_argument('offset', type=int, help='Offset to add')
-args = parser.parse_args()
-input_path = args.input_path
-offset = args.offset
+def parse_args():
+    '''
+    Parser for the arguments.
 
-os.chdir(input_path)
+    Returns
+    ----------
+    args : obj
+        The arguments.
 
-for fname in os.listdir():
-    if os.path.isdir(fname): continue
-    if not fname.split('.')[-1] == 'png': continue
+    '''
+    parser = argparse.ArgumentParser('Rename images by adding the offset.')
+    parser.add_argument('--input_dir', type=str,
+                        help='''Input directory containing the images to
+                                rename.''')
+    parser.add_argument('--offset', type=int,
+                        help='Offset to add.')
+    args = parser.parse_args()
+    return args
 
-    frame = int(fname.split('.')[0].split('_')[-1])
-    frame = frame + offset
-    out_fname = "pano_" + "{:06d}".format(frame) + '.png'
-    os.rename(fname, out_fname)
+
+if __name__ == '__main__':
+    # Load the arguments
+    args = parse_args()
+    input_dir = args.input_dir
+    offset = args.offset
+
+    # Find image file names.
+    fnames = glob.glob(input_dir+'/*.jpg')
+    for fname in fnames:
+        # Rename images by adding the offset.
+        frame_num = int(fname.split('/')[-1].split('.')[0].split('_')[-1])
+        frame_num = frame_num + offset
+        out_fname = 'pano_' + '{:06d}'.format(frame_num) + '.jpg'
+        out_fname = os.path.join(input_dir, out_fname)
+        os.rename(fname, out_fname)
