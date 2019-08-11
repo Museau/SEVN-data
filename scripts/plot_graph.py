374b7412e2651a07134d09b0b8d941b3438a7265
Museau <margaux.luck@gmail.com>
2019-08-12 01:17:55 +0200

    add parse_args/main/comments

--- scripts/plot_graph.py
+++ scripts/plot_graph.py
@@ -1,72 +1,105 @@
 import os
-from shutil import copyfile
-import networkx as nx
-import matplotlib.pyplot as plt
-import subprocess
+
 import argparse
-import pandas as pd
 import collections
+import matplotlib.pyplot as plt
+import networkx as nx
+import pandas as pd
 
-parser = argparse.ArgumentParser("Creates panoramas from vuze pictures")
-parser.add_argument('--input_path', type=str, help='Input path')
-parser.add_argument('--zones', action='store_true', help='Different color for streets and corners')
-args = parser.parse_args()
 
-print("Path : " + args.input_path)
-input_path = args.input_path
-zones = args.zones
+def parse_args():
+    '''
+    Parser for the arguments.
 
-G = nx.read_gpickle(input_path + '/graph.pkl')
+    Returns
+    ----------
+    args : obj
+        The arguments.
 
-pos = {k: v.get("coords")[0:2] for k, v in G.nodes(data=True)}
-if zones:
-    meta_df = pd.read_hdf(input_path + "/meta.hdf5", key="df", index=False)
-    corners = [node for node in G.nodes if node in meta_df[meta_df.type == 'intersection'].frame]
-    streets = [node for node in G.nodes if node in meta_df[meta_df.type == 'street_segment'].frame]
-    other = [node for node in G.nodes if node not in meta_df[meta_df.type == 'street_segment'].frame]
-    other = [node for node in other if node not in meta_df[meta_df.type == 'intersection'].frame]
+    '''
+    parser = argparse.ArgumentParser('Creates panoramas from vuze pictures.')
+    parser.add_argument('--input_dir',
+                        type=str,
+                        help='Input directory containing the .pkl graph.')
+    parser.add_argument('--zones',
+                        action='store_true',
+                        help='Different color for streets and corners.')
+    args = parser.parse_args()
+    return args
 
-    nx.draw_networkx_nodes(G, pos,
-                           nodelist=corners,
-                           node_color='#e23f3a',
-                           node_size=1,
-                           alpha=0.8,
-                           with_label=True)
 
-    nx.draw_networkx_nodes(G, pos,
-                           nodelist=streets,
-                           node_color='#000cda',
-                           node_size=1,
-                           alpha=0.8)
+if __name__ == '__main__':
+    # Load the arguments
+    args = parse_args()
 
-    nx.draw_networkx_nodes(G, pos,
-                           nodelist=other,
-                           node_color='g',
-                           node_size=20,
-                           alpha=0.8)
-    edges = nx.draw_networkx_edges(G, pos=pos)
+    input_dir = args.input_dir
+    print('input_dir : {}'.format(input_dir))
 
-else:
-    nx.draw(G, pos,node_color='r', node_size=1)
+    zones = args.zones
 
-plt.axis('equal')
-plt.savefig('graph.png', transparent=True, dpi=1000)
-plt.show()
+    # Load graph
+    graph_fname = os.path.join(input_dir, 'graph.pkl')
+    G = nx.read_gpickle(graph_fname)
 
-print("num_edges: "+ str(len(G.edges)))
-print("num_nodes: "+ str(len(G.nodes)))
+    pos = {k: v.get('coords')[0:2] for k, v in G.nodes(data=True)}
+    if zones:
+        # Get labels.
+        meta_fname = os.path.join(input_dir, 'meta.hdf5')
+        meta_df = pd.read_hdf(meta_fname, key='df', index=False)
+        corners = [node for node in G.nodes if node in
+                   meta_df[meta_df.type == 'intersection'].frame]
+        streets = [node for node in G.nodes if node in
+                   meta_df[meta_df.type == 'street_segment'].frame]
+        other = [node for node in G.nodes if node not in
+                 meta_df[meta_df.type == 'street_segment'].frame]
+        other = [node for node in other if node not in
+                 meta_df[meta_df.type == 'intersection'].frame]
 
-degree_sequence = sorted([d for n, d in G.degree()], reverse=True)  # degree sequence
-# print "Degree sequence", degree_sequence
-degreeCount = collections.Counter(degree_sequence)
-deg, cnt = zip(*degreeCount.items())
+        nx.draw_networkx_nodes(G, pos,
+                               nodelist=corners,
+                               node_color='#e23f3a',
+                               node_size=1,
+                               alpha=0.8,
+                               with_label=True)
 
-fig, ax = plt.subplots()
-plt.bar(deg, cnt, width=0.80, color='b')
+        nx.draw_networkx_nodes(G, pos,
+                               nodelist=streets,
+                               node_color='#000cda',
+                               node_size=1,
+                               alpha=0.8)
 
-plt.title("Degree Histogram")
-plt.ylabel("Count")
-plt.xlabel("Degree")
-ax.set_xticks([d + 0.4 for d in deg])
-ax.set_xticklabels(deg)
-plt.show()
+        nx.draw_networkx_nodes(G, pos,
+                               nodelist=other,
+                               node_color='g',
+                               node_size=20,
+                               alpha=0.8)
+
+        edges = nx.draw_networkx_edges(G, pos=pos)
+
+    else:
+        nx.draw(G, pos, node_color='r', node_size=1)
+
+    plt.axis('equal')
+    plot_fname = os.path.join(input_dir, 'colored_graph.png')
+    print('hist_fname {}:'.format(plot_fname))
+    plt.savefig(plot_fname, transparent=True, dpi=1000)
+
+    print('num_edges: {}'.format(len(G.edges)))
+    print('num_nodes: {}'.format(len(G.nodes)))
+
+    degree_sequence = sorted([d for n, d in G.degree()], reverse=True)
+    degreeCount = collections.Counter(degree_sequence)
+    deg, cnt = zip(*degreeCount.items())
+
+    # Degree histogram
+    fig, ax = plt.subplots()
+    plt.bar(deg, cnt, width=0.80, color='b')
+
+    plt.title('Degree Histogram')
+    plt.ylabel('Count')
+    plt.xlabel('Degree')
+    ax.set_xticks([d + 0.4 for d in deg])
+    ax.set_xticklabels(deg)
+    hist_fname = os.path.join(input_dir, 'degree_hist.png')
+    print('hist_fname {}:'.format(hist_fname))
+    plt.savefig(hist_fname)
