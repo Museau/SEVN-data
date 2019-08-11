1c36f52213f1072ae666644216dc12b512bb0792
Museau <margaux.luck@gmail.com>
2019-08-11 15:16:08 +0200

    add parse_args/main/comments

--- scripts/write_times.py
+++ scripts/write_times.py
@@ -1,16 +1,37 @@
-"""Writes timestamps file for ORBSLAM2"""
-
 import argparse
 
-parser = argparse.ArgumentParser("Writes timestamp file for ORBSLAM2")
-parser.add_argument('fps', type=float, help='fps')
-parser.add_argument('file_len', type=int, help='number of images')
-args = parser.parse_args()
-fps = args.fps
-file_len = file_len
 
-file = open("times.txt","a")
-for i in range(file_len): 
-    out_fname = "{:06f}".format((i+1)/fps)
-    file.write(out_fname+"\n")
-file.close()
+def parse_args():
+    '''
+    Parser for the arguments.
+
+    Returns
+    ----------
+    args : obj
+        The arguments.
+
+    '''
+    parser = argparse.ArgumentParser('Writes timestamp file for ORBSLAM2.')
+    parser.add_argument('--output_fname', type=str,
+                        help='Output .txt file name.')
+    parser.add_argument('--file_len', type=int,
+                        help='Number of images.')
+    parser.add_argument('--fps', type=float, default=30,
+                        help='Frame per second.')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    # Load the arguments
+    args = parse_args()
+    output_fname = args.output_fname
+    fps = args.fps
+    file_len = args.file_len
+
+    # Write timestamp file.
+    file = open(output_fname, 'a')
+    for i in range(1, file_len+1):
+        out_fname = '{:06f}'.format(i/fps)
+        file.write(out_fname+'\n')
+    file.close()
