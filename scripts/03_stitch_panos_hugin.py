ec16f2c677a895ad83bff7367cad414a01f97e0c
Museau <margaux.luck@gmail.com>
2019-08-11 11:51:42 +0200

    add parse_args/main/comments

--- scripts/03_stitch_panos_hugin.py
+++ scripts/03_stitch_panos_hugin.py
@@ -1,31 +1,63 @@
-"""Stitch panoramas"""
+'''Stitch panoramas'''
 
 import os
-from shutil import copyfile
-import subprocess
+
 import argparse
+import glob
+import subprocess
 
-parser = argparse.ArgumentParser("Creates panoramas from vuze pictures")
-parser.add_argument('input_path', type=str,
-                    help='folder containing all images')
-args = parser.parse_args()
+from scripts.utils import check_dir
 
-print("Path : " + args.input_path)
-input_path = args.input_path
 
-if not os.path.exists(input_path + '/../panoramas'):
-    os.makedirs(input_path + '/../panoramas')
+def parse_args():
+    '''
+    Parser for the arguments.
 
-output_dir = input_path + '/../panoramas'
+    Returns
+    ----------
+    args : obj
+        The arguments.
 
-for fname in os.listdir(input_path + '/image_1/'):
-    frame = fname.split('.')[0]
-    if not (os.path.exists(output_dir + frame + '.png')):
-        try:
-            cmd = ['./scripts/stitch.sh', input_path, output_dir, frame]
-            print(cmd)
-            print(subprocess.check_output(cmd))
-        except KeyboardInterrupt:
-            break
-        except:
-            pass
+    '''
+    parser = argparse.ArgumentParser('''Creates panoramas from vuze frame
+                                        views.''')
+    parser.add_argument('--input_dir',
+                        type=str,
+                        help='Input directory containing frame views.')
+    parser.add_argument('--output_dir',
+                        type=str,
+                        help='Output directory of created panoramas.')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    # Load the arguments
+    args = parse_args()
+
+    input_dir = args.input_dir
+    print('input_dir: {}'.format(input_dir))
+    # Check if input directory exist and is not empty.
+    check_dir(input_dir)
+
+    output_dir = args.output_dir
+    print('output_dir: {}'.format(output_dir))
+    # Make sure output directory exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Iterate of video frames.
+    for file in glob.glob(input_dir+'/camera_0/*.jpg'):
+        frame_num = file.split('/')[-1].split('.')[0]
+        out_fname = os.path.join(output_dir, frame_num + '.jpg')
+        # Continue if file already exist
+        if not os.path.isfile(out_fname):
+            try:
+                # Run './scripts/stitch.sh' for each video frame.
+                cmd = ['./scripts/stitch.sh', input_dir, output_dir, frame_num]
+                print(cmd)
+                print(subprocess.check_output(cmd))
+            except KeyboardInterrupt:
+                break
+            except:
+                pass
