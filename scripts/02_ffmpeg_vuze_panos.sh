1f7b088859a4d5df485eb66a2d69c291e40a1f97
Museau <margaux.luck@gmail.com>
2019-08-11 11:49:42 +0200

    rewrite step 2 bash script

--- scripts/02_ffmpeg_vuze_panos.sh
+++ scripts/02_ffmpeg_vuze_panos.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-# $1 = video path
-# $2 = destination folder
+VUZE_FOLDER='data/SEVN/raw/vuze'
 
-mkdir -p $2
-ffmpeg -i $1 -r 30 -map 0:0 "$2/pano_%06d.png"
-
-
-
-
-
-
-
-
+# Get the .MP4 in the 360 degree stabilized video sub-folder of each date of data acquisition.
+for VIDEO in $VUZE_FOLDER/*/stabilized_video/*.MP4; do
+  F=`echo ${VIDEO/video/panorama} | cut -d'.' -f1`
+  echo 'video file: '$F
+  mkdir -p ${F%/*}
+  # Videos to image sequence (30 frames per seconde).
+  ffmpeg -i $VIDEO -r 30 -map 0:0 $F'_%06d.jpg'
+done
