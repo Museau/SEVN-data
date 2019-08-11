38c2f644514c651f6cf8c8bf82157a26ca76f527
Museau <margaux.luck@gmail.com>
2019-08-11 12:38:52 +0200

    add parse_args/main/comments

--- scripts/04_stitch_reconstructions.py
+++ scripts/04_stitch_reconstructions.py
@@ -1,33 +1,160 @@
 import os
-import sys
-import struct
-import utils
+
+import argparse
+import glob
 import numpy as np
 import pandas as pd
-import quaternion
+
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import argparse
 
-parser = argparse.ArgumentParser("Extracts camera positions from ORBSLAM2 txt file")
-parser.add_argument('input_path', type=str, help='Input path')
-parser.add_argument('--plot', action='store_true', help='Plot poses')
-
-args = parser.parse_args()
+from scripts.utils import check_dir, read_cameras_text
 
 
-ROT_MAT = np.array([[0, 0, 1], [-1, 0, 0], [0, -1, 0]])
-FPS = 30
+def parse_args():
+    '''
+    Parser for the arguments.
+
+    Returns
+    ----------
+    args : obj
+        The arguments.
+
+    '''
+    parser = argparse.ArgumentParser('''Extracts camera positions from ORBSLAM2
+                                    .txt files and stitch them together.''')
+    parser.add_argument('--input_dir',
+                        type=str,
+                        help='Input directory containg the .txt files.')
+    parser.add_argument('--output_dir',
+                        type=str,
+                        help='''Output directory containg the .csv file with
+                                the stitched camera positions.''')
+    parser.add_argument('--plot',
+                        action='store_false',
+                        help='Plot poses.')
+    args = parser.parse_args()
+    return args
 
 
 class Pose():
     def __init__(self, timestamp, t, angle):
+        '''
+        Pose class.
+
+        Parameters
+        ----------
+        timestamp : float
+            Timestamp.
+        t: numpy array
+            Array of x, y, z coordinates.
+        angle: float
+            Angle.
+
+        '''
         self.timestamp = timestamp
         self.t = t
         self.angle = angle
 
 
+def rotate_poses(poses, angle):
+    '''
+    Rotate poses in a list.
+
+    Parameters
+    ----------
+    poses: list
+        List of poses.
+    angle: float
+        Angle.
+
+    Returns
+    -------
+    poses: list
+        List of rotated poses.
+
+    '''
+    rot_point = poses[0].t
+    rot_mat = np.array([[np.cos(np.deg2rad(angle)),
+                         -np.sin(np.deg2rad(angle)), 0],
+                        [np.sin(np.deg2rad(angle)),
+                         np.cos(np.deg2rad(angle)), 0],
+                        [0, 0, 1]])
+    for pose in poses:
+        tvec = pose.t - rot_point
+        tvec = np.dot(rot_mat, tvec)
+        pose.t = rot_point + tvec
+        pose.angle = (pose.angle + angle) % 360
+    return poses
+
+
+def scale_poses(poses, ref, scale_x, scale_y):
+    '''
+    Scale poses.
+
+    Parameters
+    ----------
+    poses: list
+        List of poses to scale.
+    ref: list
+        List of reference poses.
+    scale_x: float
+        Scale x.
+    scale_y: float
+        Scale y.
+
+    Returns
+    -------
+    poses: list
+        List of scaled poses.
+
+    '''
+    dist = []
+    for idx in range(len(poses)-1):
+        dist.append(np.linalg.norm(poses[idx+1].t - poses[idx].t) / (
+            poses[idx+1].timestamp - poses[idx].timestamp))
+
+    ref_dist = []
+    for idx in range(len(ref)-1):
+        ref_dist.append(np.linalg.norm(ref[idx+1].t - ref[idx].t) / (
+            ref[idx+1].timestamp - ref[idx].timestamp))
+
+    avg_dist = np.average(dist)
+    avg_ref_dist = np.average(ref_dist)
+    scale_factor = avg_ref_dist / avg_dist
+
+    for pose in poses:
+        pose.t[0] = pose.t[0] * scale_factor * scale_x
+        pose.t[1] = pose.t[1] * scale_factor * scale_y
+    return poses
+
+
 def stitch(poses1, poses2, scale_x, scale_y, angle, x, y):
+    '''
+    Stitch together two list of poses.
+
+    Parameters
+    ----------
+    poses1: list
+        List of poses 1.
+    poses2: list
+        List of poses 2.
+    scale_x: float
+        Scale x.
+    scale_y: float
+        Scale y.
+    angle: float
+        Angle.
+    x: float
+        x.
+    y: float
+        y.
+
+    Returns
+    -------
+    reconstruction: list
+        List of poses 1 and 2 stitched.
+
+    '''
     poses2 = rotate_poses(poses2, angle)
     poses2 = scale_poses(poses2, poses1, scale_x, scale_y)
     intersection = poses1[-1]
@@ -38,39 +165,44 @@ def stitch(poses1, poses2, scale_x, scale_y, angle, x, y):
         reconstruction.append(pose)
     return reconstruction
 
-def scale_poses(poses, ref, scale_x, scale_y):
-    dist = []
-    ref_dist = []
-    for idx in range(len(poses)-1):
-        dist.append(np.linalg.norm(poses[idx+1].t - poses[idx].t) / (poses[idx+1].timestamp - poses[idx].timestamp))
-    for idx in range(len(ref)-1):
-        ref_dist.append(np.linalg.norm(ref[idx+1].t - ref[idx].t) / (ref[idx+1].timestamp - ref[idx].timestamp))
-    avg_dist = np.average(dist)
-    avg_ref_dist = np.average(ref_dist)
-    scale_factor = avg_ref_dist / avg_dist
 
-    for pose in poses:
-        pose.t[0] = pose.t[0] * scale_factor *scale_x
-        pose.t[1] = pose.t[1] * scale_factor *scale_y
+def plot_data(x, y, z, l):
+    '''
+    Plot poses.
 
-    return poses
+    Parameters
+    ----------
+    x: list
+        List of x.
+    y: list
+        List of y.
+    z: list
+        List of z.
+    l: int
+        # of poses.
 
-def rotate_poses(poses, angle):
-    rot_point = poses[0].t
-    rot_mat = np.array([[np.cos(np.deg2rad(angle)), -np.sin(np.deg2rad(angle)), 0], \
-                        [np.sin(np.deg2rad(angle)), np.cos(np.deg2rad(angle)), 0],  \
-                        [0, 0, 1]])
-    for pose in poses:
-        tvec = pose.t - rot_point
-        tvec = np.dot(rot_mat, tvec)
-        pose.t = rot_point + tvec
-        pose.angle = (pose.angle + angle) % 360
-
-    return poses
+    '''
+    plt.scatter(x[0:l], y[0:l], c='blue')
+    plt.scatter(x[l:-1], y[l:-1], c='red')
+    plt.axis('equal')
+    plt.savefig(os.path.join(output_dir, 'poses.png'))
 
 
 def plot_poses(poses, l):
-    x = []; y = []; z = [];
+    '''
+    Plot poses.
+
+    Parameters
+    ----------
+    poses: list
+        List of poses.
+    l: int
+        # of poses.
+
+    '''
+    x = []
+    y = []
+    z = []
     for pose in poses:
         x.append(pose.t[0])
         y.append(pose.t[1])
@@ -78,41 +210,59 @@ def plot_poses(poses, l):
     plot_data(x, y, z, l)
 
 
-def plot_data(x, y, z, l):
-    plt.scatter(x[0:l], y[0:l], c='blue')
-    plt.scatter(x[l:-1], y[l:-1], c='red')
-    plt.axis('equal')
-    plt.show()
+if __name__ == '__main__':
+    # Load the arguments.
+    args = parse_args()
 
+    input_dir = args.input_dir
+    print('input_dir: {}'.format(input_dir))
+    # Check if input directory exist and is not empty.
+    check_dir(input_dir)
 
-folder = args.input_path
-reconstructions = {}
-for idx in range(18):
-    reconstructions["{:02d}".format(idx + 1)] = [Pose(*x) for x in utils.read_cameras_text(folder + "{:02d}".format(idx + 1) + '.txt')]
-test = stitch(reconstructions['01'], reconstructions['02'], 1.0, 1.0, -5.0, 1.0, 0.5)
-test = stitch(test, reconstructions['03'], 1.0, 1.0, -96.0, 0.0, 0.0)
-test = stitch(test, reconstructions['04'], 1.1, 1.1, 182.0, -6.0, 0.0)
-test = stitch(test, reconstructions['05'], 1.05, 1.05, 87.5, 0.0, 3.0)
-test = stitch(test, reconstructions['06'], 1.09, 1.09, -3.0, -1.0, 0.0)
-test = stitch(test, reconstructions['07'], 1.0, 1.0, -95.0, 0.0, -1.0)
-test = stitch(test, reconstructions['08'], 1.15, 1.0, -7.0, 0.0, -1.0)
-test = stitch(test, reconstructions['09'], 1.06, 1.06, -85.5, 0.0, 4.0)
-#test = stitch(test, reconstructions['10'], 1.15, 1.15, 180, 0.0, 0.0)
-test = stitch(test, reconstructions['11'], 1.0, 1.0, 90, -10.0, 220.0)
-test = stitch(test, reconstructions['12'], 1.05, 1.1, 5.0, 5.0, 0.0)
-test = stitch(test, reconstructions['13'], 1.0, 1.0, 91.0, 5.0, -12.5)
-test = stitch(test, reconstructions['14'], 1.0, 1.0, 87.0, 0.0, 3.0)
-test = stitch(test, reconstructions['15'], 1.1, 1.0, -1.0, 0.0, 0.0)
-test = stitch(test, reconstructions['16'], 1.0, 1.0, 176.0, 0.0, -1.0)
-test = stitch(test, reconstructions['17'], 1.0, 1.0, 181.5, -3.0, -2.0)
-test = stitch(test, reconstructions['18'], 1.2, 0.95, 82.0, 0.0, 0.0)
+    output_dir = args.output_dir
+    print('output_dir: {}'.format(output_dir))
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-if args.plot:
-    plot_poses(test, len(test))
+    # Load ORBSLAM2 outputs.
+    reconstructions = {}
+    for file in glob.glob(input_dir+'/*.txt'):
+        idx = file.split('.')[0].split('/')[-1]
+        reconstructions[idx] = [Pose(*x) for x in read_cameras_text(file)]
 
-res = []
-for pose in test:
-    res.append((pose.timestamp, pose.t[0], pose.t[1], pose.t[2], pose.angle))
-res = pd.DataFrame(res, columns = ["timestamp", "x", "y", "z", "angle"])
-print(args.input_path)
-res.to_csv(args.input_path + "coords.csv")
+    # Stitch ORBSLAM2 outputs.
+    poses = reconstructions['01']
+    poses = stitch(poses, reconstructions['02'], 1.0, 1.0, -5.0, 1.0, 0.5)
+    poses = stitch(poses, reconstructions['03'], 1.0, 1.0, -96.0, 0.0, 0.0)
+    poses = stitch(poses, reconstructions['04'], 1.1, 1.1, 182.0, -6.0, 0.0)
+    poses = stitch(poses, reconstructions['05'], 1.05, 1.05, 87.5, 0.0, 3.0)
+    poses = stitch(poses, reconstructions['06'], 1.09, 1.09, -3.0, -1.0, 0.0)
+    poses = stitch(poses, reconstructions['07'], 1.0, 1.0, -95.0, 0.0, -1.0)
+    poses = stitch(poses, reconstructions['08'], 1.15, 1.0, -7.0, 0.0, -1.0)
+    poses = stitch(poses, reconstructions['09'], 1.06, 1.06, -85.5, 0.0, 4.0)
+    # We don't use the reconstruction 10.
+    # poses = stitch(poses, reconstructions['10'], 1.15, 1.15, 180, 0.0, 0.0)
+    poses = stitch(poses, reconstructions['11'], 1.0, 1.0, 90, -10.0, 220.0)
+    poses = stitch(poses, reconstructions['12'], 1.05, 1.1, 5.0, 5.0, 0.0)
+    poses = stitch(poses, reconstructions['13'], 1.0, 1.0, 91.0, 5.0, -12.5)
+    poses = stitch(poses, reconstructions['14'], 1.0, 1.0, 87.0, 0.0, 3.0)
+    poses = stitch(poses, reconstructions['15'], 1.1, 1.0, -1.0, 0.0, 0.0)
+    poses = stitch(poses, reconstructions['16'], 1.0, 1.0, 176.0, 0.0, -1.0)
+    poses = stitch(poses, reconstructions['17'], 1.0, 1.0, 181.5, -3.0, -2.0)
+    poses = stitch(poses, reconstructions['18'], 1.2, 0.95, 82.0, 0.0, 0.0)
+
+    if args.plot:
+        # Plot poses.
+        plot_poses(poses, len(poses))
+
+    # Constuct pandas dataframe with the stitched camera positions.
+    res = []
+    for pose in poses:
+        res.append((pose.timestamp,
+                    pose.t[0], pose.t[1], pose.t[2],
+                    pose.angle))
+    res = pd.DataFrame(res, columns=['timestamp', 'x', 'y', 'z', 'angle'])
+    print(res)
+    # Save output.
+    print('output fname: {}'.format(os.path.join(output_dir, 'coords.csv')))
+    res.to_csv(os.path.join(output_dir, 'coords.csv'))
